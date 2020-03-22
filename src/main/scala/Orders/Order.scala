7e5c4489a851e45453fad774bfd58aa984528a39
fearlesspandas <landonrenzullo@gmail.com>
2020-03-21 22:40:29 -0400

    added example of algebraic order matching using Typical

---
+++ src/main/scala/Orders/Order.scala
@@ -0,0 +1,52 @@
+package Orders
+
+import Typical.core.Typeable.{dataset, model, number}
+import impl.{axiom, recSim, sim}
+
+import scala.reflect.ClassTag
+
+//import Typical.implicits.implicits._
+object Order {
+
+  import Typical.implicits.implicits._
+  implicit object myprovider extends number
+  class player extends axiom[player](0) with number{
+    val isplayer = true
+  }
+  class item extends axiom[item](0) with number
+//
+//  class price extends axiom[price](0) with number
+//  implicit val tx: dataset[player with item with price] => dataset[order] = (src: dataset[player with item with price]) => {
+//    val p = src.fetch[player]
+//    new order()(0)
+//  }
+
+  implicit val tx1 : dataset[two with one] => dataset[one] = (src:dataset[two with one]) => new one()(src.fetch[one].initialVal - 1d)
+  implicit val tx2 : dataset[one with two] => dataset[two] = null
+
+  class one extends recSim[one,two with one](tx1)(1d) with number
+  class two extends recSim[two,one with two](tx2)(1d) with number
+  class ordermatch[A<:model[B with A,A],B<:model[A with B,B]]( //due to the identity mapping we can always say any A is a dependency of itself
+                                                 override implicit val iterateFrom: dataset[A with B] => dataset[ordermatch[A, B]],
+                                                 override implicit val tag: ClassTag[ordermatch[A, B]]
+                                               ) extends sim[A with B,ordermatch[A,B]](0){
+    def fill() = {
+      //find owners of the A and B calculation functions
+      //call calc on A and B to fill them as specified
+      //give owners 1 of A and B respectively
+    }
+
+  }
+  implicit val matching: dataset[one with two] => dataset[ordermatch[one,two]] = null
+  //ToDo implement the following in 'matching' function
+  //and make it's structur producable by a factory.
+
+  //find owners of the A and B calculation functions
+  //call calc on A and B to fill them as specified
+  //give owners 1 of A and B respectively
+
+  new ordermatch[one,two]() //proves that one and two can fill eachother
+                                                  //calling calc on an ordermatch should then trigger the
+                                                  //the atomic fill of the order
+
+}
