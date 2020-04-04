83de1881fdf3b41309e888ad6e226a86aaff2616
fearlesspandas <landonrenzullo@gmail.com>
2020-04-03 20:41:39 -0400

    added comment

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -35,6 +35,7 @@ object Test {
     //in the future these calls to fetch src can hopefully be made implicit
     val x = src.fetchDouble[Y]
     val a = src.fetchDouble[A]
+    //example showcasing typesafe retrieval of past state data
     println("Y state:" + src.fetchFromState[Double,Y](15).typedInitVal)
     //all operations are typechecked to ensure all datasets have correct initial types
     ((x*x) + a)/(x*2)
